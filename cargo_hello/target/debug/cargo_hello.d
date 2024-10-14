5656fed10300ee5e44e2f2fe0166b0c7af4b5757
Daniel Peter <peterdanielfun24@gmail.com>
2024-10-14 14:14:37 +0100

    blockchain developer

---
+++ cargo_hello/target/debug/cargo_hello.d
@@ -0,0 +1,1 @@
+C:\Users\hp\learn_rust\cargo_hello\target\debug\cargo_hello.exe: C:\Users\hp\learn_rust\cargo_hello\src\main.rs
---
+++ cargo_hello/target/debug/deps/cargo_hello.d
@@ -0,0 +1,5 @@
+C:\Users\hp\learn_rust\cargo_hello\target\debug\deps\cargo_hello.exe: src/main.rs
+
+C:\Users\hp\learn_rust\cargo_hello\target\debug\deps\cargo_hello.d: src/main.rs
+
+src/main.rs:
